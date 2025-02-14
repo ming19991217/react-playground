1bfea6ec5708c0c05e24b417c0ed439c956ae42d
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-15 00:42:33 +0800

    feat : button animation

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -18,6 +18,10 @@ export default function Natours() {
                     <span className="heading-primary-main">Outdoors</span>
                     <span className="heading-primary-sub">is where life happens</span>
                 </h1>
+
+                <a href="#" className="btn btn-white btn-animated">
+                    Discover our tours
+                </a>
             </div>
 
 
