01487ca9025f292a2f034ee03dc81182e4919de9
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-15 00:04:08 +0800

    feat: header & title animations

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -1,12 +1,26 @@
 
 
+import logo from '../img/logo-white.png';
 
 
 export default function Natours() {
     return (
         <header className="header">
-            Some text...
-            AAA
+
+            {/* Logo */}
+            <div className="logo-box">
+                <img src={logo} alt="Logo" className='logo' />
+            </div>
+
+            {/* 標題 */}
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary-main">Outdoors</span>
+                    <span className="heading-primary-sub">is where life happens</span>
+                </h1>
+            </div>
+
+
         </header>
     );
 }
