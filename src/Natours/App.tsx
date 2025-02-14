f83be564a663d43e8bfba8b091d9e6f8eeaaed9c
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-14 23:06:51 +0800

    feat: image assets

---
+++ src/Natours/App.tsx
@@ -0,0 +1,13 @@
+import Natours from "./page/Natours"
+import "./page/style.css"
+
+function App() {
+
+  return (
+    <>
+      <Natours />
+    </>
+  )
+}
+
+export default App
---
+++ src/Natours/page/Natours.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function Natours() {
+    return (
+        <header className="header">
+            Some text...
+            AAA
+        </header>
+    );
+}
