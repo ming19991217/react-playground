bfd9390205f2bc5e5b5f336470d9768fe505351f
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-16 01:53:14 +0800

    feat: implementing BEM

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -8,18 +8,18 @@ export default function Natours() {
         <header className="header">
 
             {/* Logo */}
-            <div className="logo-box">
-                <img src={logo} alt="Logo" className='logo' />
+            <div className="header__logo-box">
+                <img src={logo} alt="Logo" className='header__logo' />
             </div>
 
             {/* 標題 */}
-            <div className="text-box">
+            <div className="header__text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary-main">Outdoors</span>
-                    <span className="heading-primary-sub">is where life happens</span>
+                    <span className="heading-primary--main">Outdoors</span>
+                    <span className="heading-primary--sub">is where life happens</span>
                 </h1>
 
-                <a href="#" className="btn btn-white btn-animated">
+                <a href="#" className="btn btn--white btn--animated">
                     Discover our tours
                 </a>
             </div>
