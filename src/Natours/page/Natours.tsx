c81992fc3b15c5681d37b887eca99a32f18cb0fb
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-19 00:52:31 +0800

    feat: Custom Grid With Floats

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -5,26 +5,82 @@ import logo from '../img/logo-white.png';
 
 export default function Natours() {
     return (
-        <header className="header">
+        <>
+            <header className="header">
 
-            {/* Logo */}
-            <div className="header__logo-box">
-                <img src={logo} alt="Logo" className='header__logo' />
-            </div>
+                {/* Logo */}
+                <div className="header__logo-box">
+                    <img src={logo} alt="Logo" className='header__logo' />
+                </div>
 
-            {/* 標題 */}
-            <div className="header__text-box">
-                <h1 className="heading-primary">
-                    <span className="heading-primary--main">Outdoors</span>
-                    <span className="heading-primary--sub">is where life happens</span>
-                </h1>
+                {/* 標題 */}
+                <div className="header__text-box">
+                    <h1 className="heading-primary">
+                        <span className="heading-primary--main">Outdoors</span>
+                        <span className="heading-primary--sub">is where life happens</span>
+                    </h1>
 
-                <a href="#" className="btn btn--white btn--animated">
-                    Discover our tours
-                </a>
-            </div>
+                    <a href="#" className="btn btn--white btn--animated">
+                        Discover our tours
+                    </a>
+                </div>
+            </header>
 
+            {/* 
+            <section className='grid-test'>
+                <div className="row">
+                    <div className="col-1-of-2">
+                        Col 1 of 2
+                    </div>
+                    <div className="col-1-of-2">
+                        Col 1 of 2
+                    </div>
+                </div>
 
-        </header>
+                <div className="row">
+                    <div className="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                    <div className="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                    <div className="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                    <div className="col-2-of-3">
+                        Col 2 of 3
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-1-of-4">
+                        Col 1 of 4
+                    </div>
+                    <div className="col-1-of-4">
+                        Col 1 of 4
+                    </div>
+                    <div className="col-2-of-4">
+                        Col 2 of 4
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-1-of-4">
+                        Col 1 of 4
+                    </div>
+                    <div className="col-3-of-4">
+                        Col 3 of 4
+                    </div>
+                </div>
+
+            </section>
+            */}
+        </>
     );
 }
