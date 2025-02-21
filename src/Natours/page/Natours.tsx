dae6764cc9140c8e78ac283ec38a17cd6a5bc33c
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-21 19:29:06 +0800

    feat: features section part

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -1,3 +1,4 @@
+import '../css/icon-font.css'
 
 
 export default function Natours() {
@@ -62,6 +63,51 @@ export default function Natours() {
                         </div>
                     </div>
 
+                </section>
+
+                <section className='section-features'>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className='feature-box__icon icon-basic-world'></i>
+                                <h3 className="heading-tertiary u-margin-bottom-small"> Explore the world </h3>
+                                <p className="feature-box__text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ipsum dolor ducimus quam nisi exercitation
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className='feature-box__icon icon-basic-compass'></i>
+                                <h3 className="heading-tertiary u-margin-bottom-small"> Meet nature </h3>
+                                <p className="feature-box__text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ipsum dolor ducimus quam nisi exercitation
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className='feature-box__icon icon-basic-map'></i>
+                                <h3 className="heading-tertiary u-margin-bottom-small"> Find your way </h3>
+                                <p className="feature-box__text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ipsum dolor ducimus quam nisi exercitation
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className='feature-box__icon icon-basic-heart'></i>
+                                <h3 className="heading-tertiary u-margin-bottom-small">  Live a healthier life </h3>
+                                <p className="feature-box__text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ipsum dolor ducimus quam nisi exercitation
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
 
