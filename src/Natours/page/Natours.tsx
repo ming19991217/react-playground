b2b32992f09981518136e13ffaf76c82558030a2
ming <47671793+ming19991217@users.noreply.github.com>
2025-02-26 23:34:51 +0800

    feat：Tours Section Done

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -122,6 +122,7 @@ export default function Natours() {
                     </div>
                     <div className="row">
                         <div className="col-1-of-3">
+                            {/* Card 1 */}
                             <div className="card">
                                 <div className="card__side card__side--front">
                                     <div className="card__picture card__picture--1">
@@ -143,17 +144,88 @@ export default function Natours() {
                                     </div>
                                 </div>
                                 <div className="card__side card__side--back card__side--back-1">
-                                    Back
+                                    <div className="card__cta">
+                                        <div className="card__price-box">
+                                            <p className="card__price-only">Only</p>
+                                            <p className="card__price-value">$297</p>
+                                        </div>
+                                        <a href="#" className="btn btn--white">Book now!</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-1-of-3">
-                            Col 1 of 3
+                            {/* Card 2 */}
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    <div className="card__picture card__picture--2">
+                                        &nbsp;
+                                    </div>
+                                    <h4 className="card__heading">
+                                        <span className="card__heading--span card__heading--span--2">
+                                            The Forest Hiker
+                                        </span>
+                                    </h4>
+                                    <div className="card__details">
+                                        <ul>
+                                            <li>7 day tours</li>
+                                            <li>Up to 40 people</li>
+                                            <li>6 tour guides</li>
+                                            <li>Sleep in provided tents</li>
+                                            <li>Difficulty: medium</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="card__side card__side--back card__side--back-2">
+                                    <div className="card__cta">
+                                        <div className="card__price-box">
+                                            <p className="card__price-only">Only</p>
+                                            <p className="card__price-value">$697</p>
+                                        </div>
+                                        <a href="#" className="btn btn--white">Book now!</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-1-of-3">
-                            Col 1 of 3
+                            {/* Card 3 */}
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    <div className="card__picture card__picture--3">
+                                        &nbsp;
+                                    </div>
+                                    <h4 className="card__heading">
+                                        <span className="card__heading--span card__heading--span--3">
+                                            The snow adventurer
+                                        </span>
+                                    </h4>
+                                    <div className="card__details">
+                                        <ul>
+                                            <li>5 day tours</li>
+                                            <li>Up to 15 people</li>
+                                            <li>3 tour guides</li>
+                                            <li>Sleep in provided tents</li>
+                                            <li>Difficulty: hard</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="card__side card__side--back card__side--back-3">
+                                    <div className="card__cta">
+                                        <div className="card__price-box">
+                                            <p className="card__price-only">Only</p>
+                                            <p className="card__price-value">$897</p>
+                                        </div>
+                                        <a href="#" className="btn btn--white">Book now!</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+                    <div className="u-center-text u-margin-top-huge">
+                        <a href="#" className="btn btn--green">Discover all tours</a>
+                    </div>
+
                 </section>
             </main>
 
