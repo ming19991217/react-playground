c267cb70cc1cd9e7b9f55008dbf7a23a0a2a9f83
ming <47671793+ming19991217@users.noreply.github.com>
2025-03-02 19:37:40 +0800

    feat： footer

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -338,6 +338,31 @@ export default function Natours() {
                 </section>
             </main>
 
+            <footer className="footer">
+                <div className="footer__logo-box">
+                    <img src="/img/logo-green-2x.png" alt="Full logo" className='footer__logo' />
+                </div>
+                <div className="row">
+                    <div className="col-1-of-2">
+                        <div className="footer__navigation">
+                            <ul className="footer__list">
+                                <li className="footer__item"> <a href="" className="footer__link"> Company </a> </li>
+                                <li className="footer__item"> <a href="" className="footer__link"> Contact us </a> </li>
+                                <li className="footer__item"> <a href="" className="footer__link"> Carrers </a> </li>
+                                <li className="footer__item"> <a href="" className="footer__link"> Privacy policy </a> </li>
+                                <li className="footer__item"> <a href="" className="footer__link"> Terms </a> </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div className="col-1-of-2">
+                        <p className="footer_copyright">
+                            Built my <a href="#" className="footer__link">Jonas Schmedtmann</a> for his online course <a href="#" className="footer__link">Advanced CSS and Sass</a>. Copyright &copy;
+                        </p>
+                    </div>
+                </div>
+            </footer>
+
             {/* 
             <section className='grid-test'>
                 <div className="row">
