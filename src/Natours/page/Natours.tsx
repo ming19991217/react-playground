af0e759d4fd23c0ad10b119c23c59fa209ae439c
ming <47671793+ming19991217@users.noreply.github.com>
2025-03-02 21:51:40 +0800

    feat： Navigation Page Done

--- src/Natours/page/Natours.tsx
+++ src/Natours/page/Natours.tsx
@@ -6,7 +6,11 @@ export default function Natours() {
         <>
             <div className="navigation">
                 <input type="checkbox" className="navigation__checkbox" id='navi-toggle' />
-                <label htmlFor="navi-toggle" className='navigation__button'>MENU</label>
+                <label htmlFor="navi-toggle" className='navigation__button'>
+                    <span className="navigation__icon">
+                        &nbsp;
+                    </span>
+                </label>
 
                 <div className="navigation__background">&nbsp;</div>
 
