87f84dcf95330b7b29c676f4a46a9371998bca59
ming <47671793+ming19991217@users.noreply.github.com>
2025-03-11 19:55:36 +0800

    feat: nav-bar

--- src/Trillo/Trillo.tsx
+++ src/Trillo/Trillo.tsx
@@ -5,7 +5,30 @@ function Trillo() {
     return (
         <div className="container">
             <header className="header">
-                Header Part
+                <img src="/trillo_img/logo.png" alt="trillo logo" className="logo" />
+
+                <form action="#" className="search">
+                    <input type="text" className="search__input" placeholder='Search hotels' />
+                    <button className="search__button">
+                        <img src="/trillo_img/SVG/magnifying-glass.svg" alt="magnifying-glass" className="search__icon" />
+                    </button>
+                </form>
+
+                <nav className="user-nav">
+                    <div className="user-nav__icon-box">
+                        <img src="/trillo_img/SVG/bookmark.svg" alt="bookmark" className="user-nav__icon" />
+                        <span className="user-nav__notification">7</span>
+                    </div>
+                    <div className="user-nav__icon-box">
+                        <img src="/trillo_img/SVG/chat.svg" alt="chat" className="user-nav__icon" />
+                        <span className="user-nav__notification">13</span>
+                    </div>
+                    <div className="user-nav__user">
+                        <img src="/trillo_img/user.jpg" alt="User photo" className="user-nav__user-photo" />
+                        <span className="user-nav__user-name">Jonas</span>
+                    </div>
+
+                </nav>
             </header>
 
             <div className="content">
