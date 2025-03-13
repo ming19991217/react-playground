e8c9c76751d34e9bcb59df1f4bd737b0655d8c82
ming <47671793+ming19991217@users.noreply.github.com>
2025-03-13 15:28:04 +0800

    feat: wip overview

--- src/Trillo/Trillo.tsx
+++ src/Trillo/Trillo.tsx
@@ -78,7 +78,54 @@ function Trillo() {
                 </nav>
 
                 <main className="hotel-view">
-                    Hotel View
+                    <div className="gallery">
+                        <figure className="gallery__item">
+                            <img src="/trillo_img/hotel-1.jpg" alt="Photo of hotel 1" className="gallery__photo" />
+                        </figure>
+                        <figure className="gallery__item">
+                            <img src="/trillo_img/hotel-2.jpg" alt="Photo of hotel 2" className="gallery__photo" />
+                        </figure>
+                        <figure className="gallery__item">
+                            <img src="/trillo_img/hotel-3.jpg" alt="Photo of hotel 3" className="gallery__photo" />
+                        </figure>
+                    </div>
+
+                    <div className="overview">
+                        <h1 className="overview__heading">
+                            Hotel Las Palmas
+                        </h1>
+                        <div className="overview__stars">
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-star" />
+                            </svg>
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-star" />
+                            </svg>
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-star" />
+                            </svg>
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-star" />
+                            </svg>
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-star" />
+                            </svg>
+                        </div>
+
+                        <div className="overview__location">
+                            <svg className="overview__icon-star">
+                                <use xlinkHref="/trillo_img/sprite.svg#icon-location-pin" />
+                            </svg>
+                            <button className="btn-inline">
+                                Albufeira, Portugal
+                            </button>
+                        </div>
+
+                        <div className="overview__rating">
+                            <div className="overview__rating-average">8.6</div>
+                            <div className="overview__rating-count">429 votes</div>
+                        </div>
+                    </div>
                 </main>
             </div>
 
